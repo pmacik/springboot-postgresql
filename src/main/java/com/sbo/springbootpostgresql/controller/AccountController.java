502d0b4565e6b3e116b3fc905d993e7323539292
Pratik Jagrut <26519653+pratikjagrut@users.noreply.github.com>
2019-09-17 17:24:43 +0530

    Update controller

--- src/main/java/com/sbo/springbootpostgresql/controller/AccountController.java
+++ src/main/java/com/sbo/springbootpostgresql/controller/AccountController.java
@@ -5,33 +5,41 @@ import com.sbo.springbootpostgresql.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
+@RequestMapping("/accounts")
 @RestController
 public class AccountController {
 
     @Autowired
     AccountService service;
 
-    @RequestMapping(value = "/accounts/{id}", method = RequestMethod.GET)
+    //@RequestMapping(value = "/accounts/{id}", method = RequestMethod.GET)
+    @GetMapping(path = "{id}")
     public ResponseEntity<Account> getAccountById(@PathVariable Long id){
         return new ResponseEntity<Account>(service.findById(id), HttpStatus.OK);
     }
 
-    @RequestMapping(value = "/accounts/", method = RequestMethod.GET)
+    //@RequestMapping(value = "/accounts/", method = RequestMethod.GET)
+    @GetMapping
     public ResponseEntity<List<Account>> getAccounts(){
         return new ResponseEntity<List<Account>>(service.getAll(), HttpStatus.OK);
     }
-    @RequestMapping(value = "/accounts/", method = RequestMethod.POST)
-    public ResponseEntity<String> createAccount(@RequestBody Account account){
+
+    //@RequestMapping(value = "/accounts/", method = RequestMethod.POST)
+    @PostMapping
+    public ResponseEntity<String> createAccount(@Valid @NonNull @RequestBody Account account){
         service.create(account);
         return new ResponseEntity<String>("Account created", HttpStatus.OK);
     }
 
-    @RequestMapping(value = "/accounts/{id}", method = RequestMethod.PUT)
-    public ResponseEntity<String > updateAccount(@PathVariable Long id, @RequestBody Account updatedAccount){
+    //@RequestMapping(value = "/accounts/{id}", method = RequestMethod.PUT)
+    @PutMapping(path = "{id}")
+    public ResponseEntity<String > updateAccount(@PathVariable Long id, @Valid @NonNull @RequestBody Account updatedAccount){
         if(service.findById(id) == null){
             return new ResponseEntity<String>("Account not found", HttpStatus.NOT_FOUND);
         }
@@ -41,7 +49,8 @@ public class AccountController {
         }
     }
 
-    @RequestMapping(value = "/accounts/{id}", method = RequestMethod.DELETE)
+    //@RequestMapping(value = "/accounts/{id}", method = RequestMethod.DELETE)
+    @DeleteMapping(path = "{id}")
     public ResponseEntity<String> deleteAccount(@PathVariable Long id){
         if(service.findById(id) == null){
             return new ResponseEntity<String>("Account not found", HttpStatus.NOT_FOUND);
